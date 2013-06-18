4f118981949b3bf6f14db4172a05a3ad60cd95ac
Eloy Espinaco <eloyesp@gmail.com>
2013-06-18 11:43:29 -0300

    Import active_attrib dirty.

---
+++ lib/active_attr/dirty.rb
@@ -0,0 +1,26 @@
+require 'active_support'
+require 'active_model/dirty'
+require 'active_attr'
+
+module ActiveAttr
+  module Dirty
+    extend ActiveSupport::Concern
+    include ActiveModel::Dirty
+
+    module ClassMethods
+      def attribute!(name, options={})
+        super(name, options)
+        define_method("#{name}=") do |value|
+          send("#{name}_will_change!") unless value == read_attribute(name)
+          super(value)
+        end
+      end
+    end
+
+    def initialize(attributes = nil, options = {})
+      super(attributes, options)
+      changed_attributes.clear
+    end
+
+  end
+end
