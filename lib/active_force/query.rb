6f492c8c1b3c9c649179ccf91b366b2af1f922dc
Pablo Oldani <oldani.pablo@gmail.com>
2014-01-15 11:28:07 -0300

    Add query builder.

---
+++ lib/active_force/query.rb
@@ -0,0 +1,61 @@
+module ActiveForce
+  class Query
+    attr_reader :table
+
+    attr_accessor :table_id
+
+    def initialize table
+      @table = table
+      @conditions = []
+      @table_id = 'Id'
+      @query_fields = [@table_id]
+    end
+
+    def fields fields_collection = []
+      @query_fields = @query_fields + fields_collection
+    end
+
+    def all
+      self
+    end
+
+    def to_s
+      query = <<-SOQL.gsub(/\s+/, " ").strip  
+        SELECT
+          #{ @query_fields.join(', ') }
+        FROM
+          #{ @table }
+        #{ build_where }
+        #{ build_limit }
+      SOQL
+      query
+    end
+
+    def where condition
+      @conditions << condition
+      self
+    end
+
+    def limit size
+      @size = size
+      self
+    end
+
+    def find id
+      where "#{ @table_id } = '#{ id }'"
+      limit 1
+      self
+    end
+
+    protected
+      def build_where
+        unless @conditions.empty?
+          "WHERE #{ @conditions.join(' AND ') }"
+        end
+      end
+
+      def build_limit
+        "LIMIT #{ @size }" if @size
+      end
+  end
+end
---
+++ spec/active_query/query_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'active_force/query'
+
+describe ActiveForce::Query do
+
+  before do
+    @query = ActiveForce::Query.new 'table_name'
+    @query.fields ['name', 'etc']
+  end
+
+  after do
+    #Object.send :remove_const, 'Client'
+  end
+
+  describe ".all" do
+    it "table should return table name" do
+      @query.all.table.should be(@query.table)
+    end
+
+    it "fields should return fields" do
+      @query.all.fields.should == @query.fields
+    end
+  end
+
+  describe ".all.to_s" do
+    it "sholud return a query for all records" do
+      @query.all.to_s.should == "SELECT Id, name, etc FROM table_name"
+    end
+  end
+
+  describe ".where" do
+    it "sholud add a where condition to a query" do
+      @query.where("name like '%a%'").to_s.should == "SELECT Id, name, etc FROM table_name WHERE name like '%a%'"
+    end
+
+    it "sholud add multiples conditions to a query" do
+      @query.where("condition1 = 1").where("condition2 = 2").to_s.should ==
+        "SELECT Id, name, etc FROM table_name WHERE condition1 = 1 AND condition2 = 2"
+    end
+  end
+
+  describe ".limit" do
+    it "sholud add a limit to a query" do
+      @query.limit("25").to_s.should == "SELECT Id, name, etc FROM table_name LIMIT 25"
+    end
+  end
+
+  describe ".find.to_s" do
+    it "sholud return a query for 1 record" do
+      @query.find(2).to_s.should == "SELECT Id, name, etc FROM table_name WHERE Id = '2' LIMIT 1"
+    end
+  end
+
+end
