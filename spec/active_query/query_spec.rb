cf2dc41a07434aaa7d9e3da05914b04c04609f9a
Pablo Oldani <oldani.pablo@gmail.com>
2014-01-15 17:26:01 -0300

    Fix typo.

--- spec/active_query/query_spec.rb
+++ spec/active_query/query_spec.rb
@@ -34,24 +34,24 @@ describe ActiveForce::Query do
   end
 
   describe ".where" do
-    it "sholud add a where condition to a query" do
+    it "should add a where condition to a query" do
       @query.where("name like '%a%'").to_s.should == "SELECT Id, name, etc FROM table_name WHERE name like '%a%'"
     end
 
-    it "sholud add multiples conditions to a query" do
+    it "should add multiples conditions to a query" do
       @query.where("condition1 = 1").where("condition2 = 2").to_s.should ==
         "SELECT Id, name, etc FROM table_name WHERE condition1 = 1 AND condition2 = 2"
     end
   end
 
   describe ".limit" do
-    it "sholud add a limit to a query" do
+    it "should add a limit to a query" do
       @query.limit("25").to_s.should == "SELECT Id, name, etc FROM table_name LIMIT 25"
     end
   end
 
   describe ".find.to_s" do
-    it "sholud return a query for 1 record" do
+    it "should return a query for 1 record" do
       @query.find(2).to_s.should == "SELECT Id, name, etc FROM table_name WHERE Id = '2' LIMIT 1"
     end
   end
