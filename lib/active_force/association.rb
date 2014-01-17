036f19c53d70bf9dcc94dd275c757f00716d64eb
Pablo Oldani <oldani.pablo@gmail.com>
2014-01-17 09:23:10 -0300

    Add a convention rails convention for foreing keys.

--- lib/active_force/association.rb
+++ lib/active_force/association.rb
@@ -4,16 +4,21 @@ module ActiveForce
   module Association
     module ClassMethods
       def has_many relation_name, options = {}
-        define_method "#{ relation_name }_query".to_sym do
-          relation_table   = relation_name.to_s.singularize.capitalize.constantize
-          association_name = options[:table] || relation_table.table_name || "#{ relation_table }__c"
-          foreing_key      = options[:foreing_key] || table_name
+        relation_model   = relation_name.to_s.singularize.capitalize.constantize
+        association_name = options[:table] || relation_model.table_name || "#{ relation_model }__c"
+        foreing_key      = options[:foreing_key] || default_foreing_key(relation_model, self.name) || table_name
+
+        define_method "#{ relation_name }_query" do
           query = ActiveForce::Query.new(association_name)
-          query.fields relation_table.fields
+          query.fields relation_model.fields
           query.where(options[:where]) if options[:where]
           query.where("#{ foreing_key } = '#{ id }'")
           query
         end
+      end
+
+      def default_foreing_key relation_model, model
+        relation_model.mappings["#{model.downcase}_id".to_sym]
       end
     end
   end
