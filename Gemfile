cefd7754a5e18e88181969953cc6233cc323f481
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-06 19:59:02 -0300

    Add gems for test coverage.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in active_force.gemspec
+gem "codeclimate-test-reporter", group: :test, require: nil
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_force'
 Dir["./spec/support/**/*"].sort.each { |f| require f }
