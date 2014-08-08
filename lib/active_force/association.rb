0815ef149d9b38b0410b7a3699392e6d417bbe1a
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-08 11:44:45 -0300

    Create different classes for associations.

--- lib/active_force/association.rb
+++ lib/active_force/association.rb
@@ -1,28 +1,18 @@
 require 'active_force/query'
+require 'active_force/association/association'
 require 'active_force/association/has_many_association'
+require 'active_force/association/belongs_to_association'
 
 module ActiveForce
   module Association
     module ClassMethods
 
       def has_many relation_name, options = {}
-        HasManyAssociation.new self, relation_name, options
-      end
-
-      def relation_model sym
-        sym.to_s.singularize.camelcase.constantize
-      end
-
-      def default_foreign_key relation_model, model
-        relation_model.mappings["#{model.downcase}_id".to_sym]
+        HasManyAssociation.new(self, relation_name, options)
       end
 
       def belongs_to relation_name, options = {}
-        model = options[:model] || relation_model(relation_name)
-        foreign_key      = options[:foreign_key] || "#{ relation_name }_id".to_sym
-        define_method "#{ relation_name }" do
-          model.find(self.send foreign_key)
-        end
+        BelongsToAssociation.new(self, relation_name, options)
       end
 
     end
