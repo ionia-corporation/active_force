840c8691d75f10230dde4274135ef5b3799eb296
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-12 17:51:41 -0300

    Refactor has_many association.

--- lib/active_force/association/has_many_association.rb
+++ lib/active_force/association/has_many_association.rb
@@ -8,29 +8,13 @@ module ActiveForce
 
       private
 
-      def build
-        super
-        define_query_method
-      end
-
-      def define_query_method
+      def define_relation_method
         association = self
-        @parent.send :define_method, query_method_name do
+        @parent.send :define_method, @relation_name do
           query = association.relation_model.query
           query.options association.options
           query.where "#{ association.foreign_key } = '#{ self.id }'"
         end
-      end
-
-      def define_relation_method
-        association = self
-        @parent.send :define_method, @relation_name do
-          association.relation_model.send_query send(association.query_method_name)
-        end
-      end
-
-      def query_method_name
-        "#{ @relation_name }_query"
       end
     end
   end
