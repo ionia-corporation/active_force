98252179ee6800f3056b3613a5fb83fef01859da
Armando Andini <armando.andini@hotmail.com>
2014-08-15 13:20:48 -0300

    More specs for active_query.

--- spec/active_force/active_query_spec.rb
+++ spec/active_force/active_query_spec.rb
@@ -6,6 +6,7 @@ describe ActiveForce::ActiveQuery do
     sobject = double("sobject")
     sobject.stub(:table_name).and_return "table_name"
     sobject.stub(:fields).and_return []
+    sobject.stub(:mappings).and_return({field: "Field__c"})
     sobject
   }
 
@@ -33,6 +34,27 @@ describe ActiveForce::ActiveQuery do
       result = @active_query.where("Text_Label = 'foo'").where("Checkbox_Label = true").to_a
       expect(result).to be_a Array
     end
+  end
 
+  describe "condition mapping" do
+    it "maps conditions for a .where" do
+      @active_query.where(field: 123)
+      expect(@active_query.to_s).to eq("SELECT Id FROM table_name WHERE Field__c = 123")
+    end
+
+    it "encloses the value in quotes if it's a string" do
+      @active_query.where field: "hello"
+      expect(@active_query.to_s).to end_with("Field__c = 'hello'")
+    end
+      
+
+  end
+
+  describe "#find_by" do
+    it "should query the client, with the SFDC field names and correctly enclosed values" do
+      expect(client).to receive :query
+      @active_query.find_by field: 123
+      expect(@active_query.to_s).to eq "SELECT Id FROM table_name WHERE Field__c = 123 LIMIT 1"
+    end
   end
 end
