b4f3ab4e7295033b6828fc245fb8f0d64ecc90b3
Jose Piccioni <josepiccioni@gmail.com>
2014-08-22 13:43:59 -0300

    generator

--- lib/generators/active_force/active_force_model/active_force_model_generator.rb
+++ lib/generators/active_force/active_force_model/active_force_model_generator.rb
@@ -1,22 +1,47 @@
-class ActiveForce::ActiveForceModelGenerator < Rails::Generators::NamedBase
+module ActiveForce
+  class ActiveForceModelGenerator < Rails::Generators::NamedBase
 
-  Attribute = Struct.new(:local_name, :remote_name)
+    source_root File.expand_path('../templates', __FILE__)
 
-  source_root File.expand_path('../templates', __FILE__)
-  argument :attributes, type: :array, default: [],
-    banner: "field[:sales_force_name] field[:sales_force_name]"
+    def create_model_file
 
-  def create_model_file
-    template "model.rb.erb", "app/models/#{file_name}.rb"
-  end
+      sf_field_names = list_field_names file_name.capitalize
+      @attributes = create_attributes sf_field_names
 
-  protected
-
-  def parse_attributes! #:nodoc:
-    self.attributes = (attributes || []).map do |attr|
-      name, sales_force_name = attr.split ':', 2
-      remote_name = sales_force_name.presence || name
-      Attribute.new name, remote_name
+      template "model.rb.erb", "app/models/#{file_name}.rb"
     end
+
+    protected
+
+    Attribute = Struct.new(:local_name, :remote_name)
+
+    def create_attributes sf_field_names
+      sf_field_names.map do |field|
+        Attribute.new( sf_name_to_symbol(field) ,field)
+      end
+    end
+
+    def list_field_names table_name
+      Client.describe(table_name).fields.map do |field|
+        field.name
+      end
+    end
+
+    def sf_name_to_symbol sf_name
+      sf_name = sf_name.underscore
+      sf_name = sf_name[0..-4] if sf_name.include? "__c"
+      sf_name.to_sym
+    end
+
+    class String
+      def underscore
+        self.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+      end
+    end
+
   end
 end
