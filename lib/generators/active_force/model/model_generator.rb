0205485baee6eb9ad82622d3a33b71facf9521df
Armando Andini <armando.andini@hotmail.com>
2014-09-01 11:11:22 -0300

    Refactor.

--- lib/generators/active_force/model/model_generator.rb
+++ lib/generators/active_force/model/model_generator.rb
@@ -7,31 +7,34 @@ module ActiveForce
     def create_model_file
       @table_name = file_name.capitalize
       @class_name = @table_name.gsub '__c', ''
-      @attributes = create_attributes 
-      template "model.rb.erb", "app/models/#{@class_name.downcase}.rb"
+      template "model.rb.erb", "app/models/#{@class_name.downcase}.rb" if table_exists?
     end
 
     protected
 
-    Attribute = Struct.new :local_name, :remote_name
+    Attribute = Struct.new :field, :column
 
-    def create_attributes 
-      sfdc_field_names.map do |field|
-        Attribute.new sf_name_to_symbol(field) ,field
+    def attributes 
+      @attributes ||= sfdc_columns.map do |column|
+        Attribute.new column_to_field(column), column
       end
     end
 
-    def sfdc_field_names 
+    def sfdc_columns
       begin
-        ActiveForce::SObject.sfdc_client.describe(@table_name).fields.map do |field|
+        @columns ||= ActiveForce::SObject.sfdc_client.describe(@table_name).fields.map do |field|
           field.name
         end
-      rescue 
+      rescue Faraday::Error::ResourceNotFound
         puts "The specified table name is not found. Be sure to append __c if it's custom"
       end
     end
 
-    def sf_name_to_symbol sf_name
+    def table_exists?
+      !! sfdc_columns
+    end
+
+    def column_to_field column
       sf_name = sf_name.underscore
       sf_name = sf_name[0..-4] if sf_name.include? "__c"
       sf_name.to_sym
