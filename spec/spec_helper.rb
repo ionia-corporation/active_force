30bf520963d7b0eab8868508cc06c3f19ff57de1
Armando Andini <armando.andini@hotmail.com>
2014-09-04 15:05:04 -0300

    Spec for eager loading.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,19 @@ require 'active_force'
 Dir["./spec/support/**/*"].sort.each { |f| require f }
 require 'pry'
 
-class Territory < ActiveForce::SObject; end
-class Quota < ActiveForce::SObject
-  field :id, from: 'Bar_Id__c'
-end
-
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.plural 'quota', 'quotas'
   inflect.singular 'quota', 'quota'
+  inflect.plural 'territory', 'territories'
+  inflect.singular 'territory', 'territory'
 end
+
+class Territory < ActiveForce::SObject
+  field :quota_id, from: "Quota__c"
+  belongs_to :quota
+end
+class Quota < ActiveForce::SObject
+  field :id, from: 'Bar_Id__c'
+  has_many :territories
+end
+
